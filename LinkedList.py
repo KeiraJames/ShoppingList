f92a10667698a1eed8553e2afb0469833a5e8735
Keira James <121837616+KeiraJames@users.noreply.github.com>
2023-12-10 05:11:48 -0500

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,28 +1,34 @@
 class Node:
+    """ This is the Node class """
     def __init__(self, data):
         self.data = data
         self.next_pointer = None
 
 
 class LinkedList(Node):
-
+    """ This is the LinkedList class """
     def __init__(self):
         self.head = None
         self.size = 0
 
+    
     def is_empty(self):
         return self.size == 0
 
+    
     def get_size(self):
         return self.size
 
+    
     def get_head(self):
         return self.head.data
 
+    
     def check_range(self, index):
         if index < 0 or index > self.size-1:
             raise Exception("Index out of bounds")
 
+    
     def set(self, index, data):
         self.check_range(index)
         temp = self.head
@@ -35,6 +41,7 @@ class LinkedList(Node):
 
         return removed_data
 
+    
     def add(self, data):
         n = Node(data)
 
@@ -49,6 +56,7 @@ class LinkedList(Node):
 
         self.size += 1
 
+    
     def add_specific_index(self, data, index):
         self.check_range(index)
 
@@ -59,7 +67,6 @@ class LinkedList(Node):
             self.head = n
         else:
             temp = self.head
-
             for i in range(index):
                 temp = temp.next_pointer
 
@@ -68,6 +75,7 @@ class LinkedList(Node):
 
         self.size += 1
 
+    
     def remove_specific_index(self, index):
         self.check_range(index)
 
@@ -78,7 +86,6 @@ class LinkedList(Node):
             self.head = None
         else:
             temp = self.head
-
             for i in range(index - 1):
                 temp = temp.next_pointer
 
@@ -86,15 +93,16 @@ class LinkedList(Node):
             temp.next_pointer = temp.next_pointer.next_pointer
 
         self.size -= 1
+        
         return removed_data
 
+    
     def remove(self):
         if self.is_empty():
             print("List is empty.")
             return
 
         temp = self.head
-
         for i in range(self.size-2):
             temp = temp.next_pointer
 
@@ -104,36 +112,11 @@ class LinkedList(Node):
 
         return removed_data
 
-    def display_list(self):
-        #print("[", end="")
+    
+    def display_list(self):      
         temp = self.head
-
         count = 1
         while temp is not None:
             print(str(count) + ".\t" + str(temp.data))
             temp = temp.next_pointer
             count += 1
-
-            #if temp.next_pointer is None:
-            #    print("", end="")
-           # else:
-           #     print(", ", end="")
-
-       # print("]")
-
-
-'''ll = LinkedList()
-
-ll.add(7)
-ll.add(8)
-ll.add(10)
-ll.add(5)
-removed = ll.remove()
-print("removed: ", removed)
-
-ll.add_specific_index(data=1, index=0)
-r = ll.remove_specific_index(1)
-print("removed: ", r)
-ll.display_list()
-print(ll.get_head())
-'''
